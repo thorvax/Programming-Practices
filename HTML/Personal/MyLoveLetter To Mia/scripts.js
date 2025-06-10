b785e99a9d2b8ed4f2b666b9b35bcb22ed526f58
thorvax <bartolome.marc.whesley@gmail.com>
2025-06-10 11:35:45 +0800

    add CSS styles and JavaScript functions for interactive elements

---
+++ HTML/Personal/MyLoveLetter To Mia/scripts.js
@@ -0,0 +1,18 @@
+function toggleCursorRight() {
+    var area = document.getElementById("Right booby");
+    // if (area.style.cursor === 'grab') {
+    //     area.style.cursor = 'grabbing';
+    // } else {
+    //     area.style.cursor = 'grab';
+    // }
+    alert("You are not allowed to touch my right boob, only the left one is allowed to be touched.");
+}
+function toggleCursorLeft() {
+    var area = document.getElementById('Left booby');
+    // if (area.style.cursor === 'grab') {
+    //     area.style.cursor = 'grabbing';
+    // } else {
+    //     area.style.cursor = 'grab';
+    // }
+    alert("You are not allowed to touch my left boob, only the right one is allowed to be touched.");
+}
